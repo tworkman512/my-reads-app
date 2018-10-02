7da707e962b1b216402f87d563fb8cee7bf9877e
Tom Workman <tomworkman@Toms-MacBook-Pro.local>
2018-10-02 17:05:17 -0500

    make OpenSearch stateless func component

--- src/components/OpenSearch.js
+++ src/components/OpenSearch.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class OpenSearch extends Component {
-  render() {
-    return (
-      <div className="open-search">
-        <Link to='/search'>Add a book</Link>
-      </div>
-    )
-  }
+const OpenSearch = () => {
+  return (
+    <div className="open-search">
+      <Link
+        to='/search'
+      >
+        Add a book
+      </Link>
+    </div>
+  )
 }
 
 export default OpenSearch
