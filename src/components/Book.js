03403490d461c7e5f8b58a261b7f1b4794633e46
Tom Workman <tomworkman@Toms-MacBook-Pro.local>
2018-10-03 21:31:25 -0500

    adds BookShelfChanger component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
+import BookShelfChanger from './BookShelfChanger'
 
 export default class Book extends Component {
   // static propTypes = {
@@ -17,15 +18,7 @@ export default class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.bookImage})` }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            <BookShelfChanger />
           </div>
           <div className="book-title">{this.props.bookTitle}</div>
           <div className="book-authors">{this.props.bookAuthor}</div>
