5f63d37540f1e842ddcc9998df638794da0e059d
Tom Workman <tomworkman@Toms-MacBook-Pro.local>
2018-10-09 22:10:09 -0500

    make book a PureComponent

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-export default class Book extends Component {
+export default class Book extends PureComponent {
 
   render() {
     const { book, updateBook, bookTitle, bookAuthor, bookImage } = this.props
